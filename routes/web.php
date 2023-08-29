115d734e1dadda72136f447427c203855f5c5774
alihumdard <alihumdard125@gmail.com>
2023-08-29 19:58:33 +0500

    Basic layout in laravel of AMS

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('quick-dashboard');
+});
+
+Route::get('/invoiceManage', function () {
+    return view('invoice-management');
+});
+
+Route::get('/invoiceSelection', function () {
+    return view('invoice-selection');
+});
+
+Route::get('/qouteManage', function () {
+    return view('qoutes-management');
+});
+
+Route::get('/qouteSelection', function () {
+    return view('qoutes-selection');
+});
+
+Route::get('/contractManage', function () {
+    return view('contracts-management');
+});
+
+Route::get('/contractSelection', function () {
+    return view('contracts-selection');
+});
+
+//users ...
+Route::get('/editAccount', function () {
+    return view('edit-account');
+});
+
+Route::get('/addAdmin', function () {
+    return view('admins-management');
+});
+
+Route::get('/addUser', function () {
+    return view('users-management');
 });
